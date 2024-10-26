c2ab10b718f2a9916a77ff4c588eea04f7c7531a
Josh Trujillo <josh.trujillo0@gmail.com>
2024-10-25 20:56:11 -0600

    first commit

---
+++ enigma.py
@@ -0,0 +1,17 @@
+'''
+enigma.py
+enigma machine simulator
+@author Josh Trujillo
+'''
+
+# encrypts a message with the given key
+def encrypt(plaintext, key):
+    pass
+
+# decrypts a message with the given key
+def decrypt(ciphertext, key):
+    pass
+
+# sets the plugboard
+def plugboard(plugs):
+    pass
