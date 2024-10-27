515f8748ff40c4b8fa11443d61b03b1c2d6332e3
Josh Trujillo <josh.trujillo0@gmail.com>
2024-10-27 10:16:31 -0600

    added encrypt_letter() and encrypt_message()

--- enigma.py
+++ enigma.py
@@ -9,15 +9,32 @@ from reflector import Reflector
 from plugboard import Plugboard
 
 class EnigmaMachine:
-    def __init__(self, rotors, reflector, plugboard):
-        sefl.rotors = rotors
-        self.reflector = reflector
-        self.plugboard = plugboard
+    def __init__(self, rotors, initial_positions, reflector, plugboard):
+        self.rotors = [Rotor(rotor, initial_positions(i)) for rotor, i in enumerate(rotors)]
+        self.reflector = Reflector(reflector)
+        self.plugboard = Plugboard(plugboard)
 
     def encrypt_letter(self, letter):
         # encrypt a single letter
-        pass
+        letter = self.plugboard.swap(letter)
+        self.step_rotors()
+        for rotor in self.rotors:
+            letter = rotor.forward_substitute(letter)
+        letter = self.reflector.reflect(letter)
+        for rotor in self.rotors:
+            letter = rotor.backward_substitute
+        return self.plugboard.swap(letter)
 
     def encrypt_message(self, message):
         # encrypt a message letter by letter
+        result = ""
+        for letter in message:
+            result += self.encrypt_letter(letter)
+        return result
+
+    def set_rotor_positions(self, positions):
+        pass
+
+    def step_rotors(self):
+
         pass
