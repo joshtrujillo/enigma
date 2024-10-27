fc2824f13ccb24972b174b3692eb180f3ea6b1df
Josh Trujillo <josh.trujillo0@gmail.com>
2024-10-27 10:14:26 -0600

    reflector wiring and reflection

--- reflector.py
+++ reflector.py
@@ -5,9 +5,18 @@ Class representing the reflector in the enigma machine
 '''
 
 class Reflector:
-    def __init__(self, wiring):
-        self.wiring = wiring
+    def __init__(self, reflector_type):
+        if reflector_type == "A":
+            self.wiring = "EJMZALYXVBWFCRQUONTSPIKHGD"
+        elif reflector_type == "B":
+            self.wiring = "YRUHQSLDPXNGOKMIEBFZCWVJAT"
+        else: # reflector C
+            self.wiring = "FVPJIAOYEDRZXWGCTKUQSBNMHL"
 
+    '''
+    reflect(self, letter)
+    returns the shifted letter based on the reflector setting
+    '''
     def reflect(self, letter):
         # reflect letter
-        pass
+        return self.wiring[ord(letter) - 65]
