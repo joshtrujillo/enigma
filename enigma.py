5ce4c1b97a75c7150b9de97d1f6cf99e85ad9f0e
Josh Trujillo <josh.trujillo0@gmail.com>
2024-10-29 21:49:04 -0600

    added main function

--- enigma.py
+++ enigma.py
@@ -10,30 +10,36 @@ from plugboard import Plugboard
 
 class EnigmaMachine:
     def __init__(self, rotors, initial_positions, reflector, plugboard):
-        self.rotors = [Rotor(rotor, initial_positions(i)) for rotor, i in enumerate(rotors)]
+        self.rotors = [Rotor(rotor, position) for rotor, position in zip(rotors, initial_positions)]
         self.reflector = Reflector(reflector)
         self.plugboard = Plugboard(plugboard)
 
     def encrypt_letter(self, letter):
-        # encrypt a single letter
+        '''
+        passes a letter to the plugboard, rotors, reflector,
+        back to rotors and the plugboard
+        '''
         letter = self.plugboard.swap(letter)
         self.step_rotors()
         for rotor in self.rotors:
             letter = rotor.forward_substitute(letter)
         letter = self.reflector.reflect(letter)
         for rotor in self.rotors:
-            letter = rotor.backward_substitute
+            letter = rotor.backward_substitute(letter)
         return self.plugboard.swap(letter)
 
     def encrypt_message(self, message):
-        # encrypt a message letter by letter
+        '''
+        encrypts a message using encrypt_letter
+        '''
         result = ""
         for letter in message:
             result += str(self.encrypt_letter(letter))
         return result
 
     def set_rotor_positions(self, positions):
-       pass
+        for position, rotor in zip(positions, self.rotors):
+            rotor.position = position
 
     def step_rotors(self):
         '''
@@ -42,3 +48,15 @@ class EnigmaMachine:
         if self.rotors[0].rotate():
             if self.rotors[1].rotate():
                 self.rotors[2].rotate()
+
+
+if __name__=="__main__":
+    rotors = ["I", "II", "III"] 
+    positions = [0, 0, 0]
+    reflector = "A"
+    plugboard = [("A", "V")]
+    machine = EnigmaMachine(rotors, positions, reflector, plugboard)
+    
+    message = "HELLOWORLD"
+    print(machine.encrypt_message(message))
+
