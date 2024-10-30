5bcf847355e583d1e1bc8e93bae6cb66ca332c34
Josh Trujillo <josh.trujillo0@gmail.com>
2024-10-29 21:49:46 -0600

    fixed the wiring dictionary

--- plugboard.py
+++ plugboard.py
@@ -6,11 +6,15 @@ Class representing the plugboard in the enigma machine
 
 class Plugboard:
     def __init__(self, wiring):
-        self.wiring = wiring
+        self.wiring = {}
+        for pair in wiring:
+            print(pair)
+            self.wiring[pair[0]] = pair[1]
+            self.wiring[pair[1]] = pair[0]
 
     def swap(self, letter):
         # swap letter based on plugboard wiring
         if letter in self.wiring:
-            return self.wiring(letter)
+            return self.wiring[letter]
         else:
             return letter
