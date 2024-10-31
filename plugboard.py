8939278588983c78198f82b195d1c0ea5078cfc7
Josh Trujillo <josh.trujillo0@gmail.com>
2024-10-30 19:38:56 -0600

    changed plugboard arg to string with letter pairs

--- plugboard.py
+++ plugboard.py
@@ -8,17 +8,18 @@ class Plugboard:
     def __init__(self, wiring):
         '''
         Args:
-            wiring: List of tuple pairs with chars to substitute.
+            wiring: String of plug pairs separated by spaces e.g. "AD FV SE"
 
         Attributes:
             wiring: Dicionary with plug mappings.
                 Two key:value pairs for each plug.
         '''
         self.wiring = {}
-        for pair in wiring:
-            print(pair)
-            self.wiring[pair[0]] = pair[1]
-            self.wiring[pair[1]] = pair[0]
+        plug_list = wiring.split()
+        for pair in plug_list:
+            if pair[0] not in self.wiring:
+                self.wiring[pair[0]] = pair[1]
+                self.wiring[pair[1]] = pair[0]
 
     def swap(self, letter):
         '''
