6aa0fee001db45fb24a13959a948a7c85c2b019f
Josh Trujillo <josh.trujillo0@gmail.com>
2024-10-31 10:30:49 -0600

    added flask app

---
+++ server.py
@@ -0,0 +1,14 @@
+"""
+server.py
+Webserver for Enigma python project
+@author Josh Trujillo
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
